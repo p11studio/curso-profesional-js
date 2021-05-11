f14d43b3daf00c321ab87eb1968690bc7b063ab4
Hugo Ponce <p11studyo@gmail.com>
2021-05-11 13:47:56 -0500

    Actualizacion clase 6 Clouseres

---
+++ assets/index.js
@@ -0,0 +1,27 @@
+const video = document.querySelector("video")
+      const button = document.querySelector("button")
+
+      function MediaPlayer(config) {
+          this.media = config.el
+      }
+
+      MediaPlayer.prototype.play = function() {
+          this.media.play()
+
+      }
+      MediaPlayer.prototype.pause = function() {
+          this.media.pause()
+
+      }
+      MediaPlayer.prototype.togglePlay = function() {
+          if (this.media.paused) {
+            this.media.play()
+          } else {
+            this.media.pause()
+          }
+
+      }
+
+      const player = new MediaPlayer({ el:video })
+      button.onclick = () => player.togglePlay()
+
