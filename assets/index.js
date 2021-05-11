6b4f3d716a82a3f32a343aefd69008bc5fa701b1
Hugo Ponce <p11studyo@gmail.com>
2021-05-11 15:22:20 -0500

    Actualizacion y resolicion de archivos con conflictos para visualizar correctamente el media player del proyecto del curso

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,7 @@
-const video = document.querySelector("video")
-      const button = document.querySelector("button")
+import MediaPlayer from './MediaPlayer.js';
 
-      function MediaPlayer(config) {
-          this.media = config.el
-      }
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video });
 
-      MediaPlayer.prototype.play = function() {
-          this.media.play()
-
-      }
-      MediaPlayer.prototype.pause = function() {
-          this.media.pause()
-
-      }
-      MediaPlayer.prototype.togglePlay = function() {
-          if (this.media.paused) {
-            this.media.play()
-          } else {
-            this.media.pause()
-          }
-
-      }
-
-      const player = new MediaPlayer({ el:video })
-      button.onclick = () => player.togglePlay()
-
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
